1071e04f5a18f0fc082d99926ef177ae790ec5a3
tampadff <csamayoac7@miumg.edu.gt>
2025-03-31 18:51:59 -0600

    xx

--- Inteligencia Artificial/python/comparararray.py
+++ Inteligencia Artificial/python/comparararray.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# Arreglos A y B con 10 números definidos por mí
 A = np.array([15, 22, 38, 45, 10, 56, 78, 64, 23, 89])
 B = np.array([15, 22, 38, 46, 10, 56, 78, 64, 23, 89])
 
@@ -8,7 +7,6 @@ B = np.array([15, 22, 38, 46, 10, 56, 78, 64, 23, 89])
 print("Arreglo A:", A)
 print("Arreglo B:", B)
 
-# Verificar si los arreglos son iguales
 if np.array_equal(A, B):
     print("Los arreglos son iguales.")
 else:
